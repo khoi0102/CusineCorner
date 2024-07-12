779bd41f8cc90e59af454f92340795d3c6680ba9
Khoi <lethanhkhoi01@gmail.com>
2024-07-12 01:40:30 -0500

    Add a cartItem and function addToCart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,9 +1,14 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 import {food_list} from "../assets/assets"
 export const StoreContext = createContext(null)
 
 function StoreContextProvider(props){
-
+const [cartItem,setCartItems]=useState({});
+function addToCart(itemId){
+    if(!cartItem[itemId]){
+        setCartItems((prev)=>({...prev,[itemId]:1}))
+    }
+}
     const contextValue={
 food_list
     }
