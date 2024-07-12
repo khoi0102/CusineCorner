ec0c448ae308c32618010ab4337f70211a5fad9e
Khoi <lethanhkhoi01@gmail.com>
2024-07-12 00:32:19 -0500

    update the add to cart button and rework the Food item css

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FoodItem.css'
 import {assets} from "../../assets/assets.js";
-function FoodItem({id,name,price,description,image}) {
+
+function FoodItem({id, name, price, description, image}) {
+    const [itemCount, setItemCount] = useState(0);
     return (
         <div className={'food-item'}>
             <div className="food-item-image-container">
                 <img className='food-item-image' src={image} alt=""/>
+                {!itemCount
+                    ? <img className="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=""/>
+                    : <div className="food-item-counter">
+
+                    </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
