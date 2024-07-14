9dbdff9a4c223b75ca67a5b5fc7f890fa0dec3b8
Khoi <lethanhkhoi01@gmail.com>
2024-07-14 02:34:59 -0500

    Create a Cart Function which allow user to add items and remove items from cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,21 +1,37 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import {food_list} from "../assets/assets"
+
 export const StoreContext = createContext(null)
 
-function StoreContextProvider(props){
-const [cartItem,setCartItems]=useState({});
-function addToCart(itemId){
-    if(!cartItem[itemId]){
-        setCartItems((prev)=>({...prev,[itemId]:1}))
+function StoreContextProvider(props) {
+    const [cartItems, setCartItems] = useState({});
+
+    /*The if statement check if the item is in the cart yet, if not, add that item in and the quantity increase to one, and if the item already in the cart, +1 the item*/
+    function addToCart(itemId) {
+        /*Create new entry*/
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({...prev, [itemId]: 1}))
+        }
+        /*Add the previous items*/
+        else {
+            setCartItems((prev) => ({...prev, [itemId]:prev[itemId] + 1}))
+        }
     }
-}
-    const contextValue={
-food_list
+
+    function removeFromCart(itemId) {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
     }
-    return(
+useEffect(()=>{
+    console.log(cartItems);
+},[cartItems])
+    const contextValue = {
+        food_list,cartItems,setCartItems,addToCart,removeFromCart
+    }
+    return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
     )
 }
+
 export default StoreContextProvider
