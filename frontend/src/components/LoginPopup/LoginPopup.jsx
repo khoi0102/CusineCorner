43053689ffddd962b1d345c1b81eb09b01ce6e89
Khoi <lethanhkhoi01@gmail.com>
2024-07-17 00:08:24 -0500

    create a login popup

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoginPopup.css'
+
+function LoginPopup() {
+    return (
+        <div>
+            <div className="login-popup">
+
+            </div>
+        </div>
+    );
+}
+
+export default LoginPopup;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import {assets} from '../../assets/assets.js';
-import { CiSearch } from "react-icons/ci";
-import { CiShoppingCart } from "react-icons/ci";
-import {useState,useEffect} from 'react'
+import {CiSearch, CiShoppingCart} from "react-icons/ci";
 
 console.log('Navbar Assets:', assets);
-function Navbar() {
-    return (
-        <div className='py-5 flex justify-between items-center  '  style={{ marginTop: '-100px', marginLeft: '-200px'}}>
-            <img src={assets.logo} alt="Logo" className='logo'/>
-            <div style={{ marginTop: '-100px'}}>
-            <ul className="flex list-none gap-20" style={{ fontSize: '20px' }}>
-                <li>home</li>
-                <li>menu</li>
-                <li>app</li>
-                <li>contact us</li>
-            </ul>
-            </div>
-            <div className="flex item-center gap-10"  style={{ marginTop: '-100px'}}>
-                   <CiSearch  className="text-4xl" style={{ position: 'relative' }}/>
-                    <CiShoppingCart className="text-4xl" />
 
-                <button className="bg-transparent border border-solid border-orange-500 p-[10px_30px] rounded-[50px] cursor-pointer hover:bg-orange-500 hover:text-white" style={{ fontSize: '16px' }}>sign in</button>
+function Navbar({setShowLogin}) {
+    const [menu, setMenu] = useState('home')
+    return (
+        <div className='navbar' style={{marginTop: '-100px', marginLeft: '-200px'}}>
+            <img src={assets.logo} alt="Logo" className='logo'/>
+            <div style={{marginTop: '-100px'}}>
+                <ul className="navbar-menu" style={{fontSize: '20px'}}>
+                    <li onClick={() => setMenu('home')} className={menu === "home" ? "active" : ""}>home</li>
+                    <li onClick={() => setMenu('menu')} className={menu === "menu" ? "active" : ""}>menu</li>
+                    <li onClick={() => setMenu('mobile-app')} className={menu === "mobile-app" ? "active" : ""}>app</li>
+                    <li onClick={() => setMenu('contact-us')} className={menu === "contact-us" ? "active" : ""}>contact
+                        us
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right" style={{marginTop: '-100px'}}>
+                <CiSearch className="search-icon" style={{position: 'relative'}}/>
+                <div className="navbar-search-icon">
+                    <CiShoppingCart className="basket-icon"/>
+                    <div className="dot"></div>
+                </div>
+                <button onClick={()=>setShowLogin(true)}>sign in</button>
             </div>
         </div>
 
