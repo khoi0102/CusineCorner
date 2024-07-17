3bdff68bb2dc85faac731e5ca9b69cfa83e7eeba
Khoi <lethanhkhoi01@gmail.com>
2024-07-16 23:09:19 -0500

    Create the app download components

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './AppDownload.css'
+import {assets} from "../../assets/assets.js";
 function AppDownload() {
     return (
-        <div>
-
+        <div className='app-download' id='app-download'>
+            <p>For Better Experience Download <br/> Cuisine Corner App</p>
+            <div className="app-download-platforms">
+                <img src={assets.play_store} alt=''/>
+                <img src={assets.app_store} alt=''/>
+            </div>
         </div>
     );
 }
