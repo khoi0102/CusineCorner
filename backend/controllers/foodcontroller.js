d013b44e67a28848fdd071365d8789d2880cf06d
Khoi <lethanhkhoi01@gmail.com>
2024-07-18 00:18:23 -0500

    create the post method to add to allow admin to add image to the frontend

--- backend/controllers/foodcontroller.js
+++ backend/controllers/foodcontroller.js
@@ -1,9 +1,24 @@
 import foodModel from "../models/foodModel.js"
-import fs from 'fs'
 
 //add food item
 
-const addFood = async(req,res)=>{
+const addFood = async (req, res) => {
+
+    let image_filename = `${req.file.filename}`
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        img:image_filename
+    })
+    try {
+        await food.save();
+        res.json({success:true,message:"Food Added"})
+    } catch (err){
+        console.log(err)
+        res.json({success:false, message:"Error"})
+    }
 
 }
-export{addFood}
+export {addFood}
